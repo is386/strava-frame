958eeaf89b8f107a6e07430621db693cd30850f0
is386 <singh.indervir89@gmail.com>
2026-02-07 12:43:26 -0500

    feat: parse monthly mileage

--- src/main.py
+++ src/main.py
@@ -26,14 +26,18 @@ weeks_ytd = days_ytd / 7
 activities_iterator = client.get_activities(after=jan_first)
 activities = list(activities_iterator)
 
-total_activities = len(activities)
-total_mileage = 0
 latest_activity = {
   'miles': activities[-1].distance / 1609.344,
   'seconds': activities[-1].moving_time
 }
 
-for activity in activities:
-  total_mileage += activity.distance / 1609.344
+total_activities = len(activities)
+total_mileage = 0
+mileage_per_month = [0] * 12
 
-print(total_activities, total_mileage, total_mileage / weeks_ytd, latest_activity)
+for activity in activities:
+  mileage = activity.distance / 1609.344
+  total_mileage += mileage
+  mileage_per_month[activity.start_date.month-1] += mileage
+
+print(total_activities, total_mileage, total_mileage / weeks_ytd, latest_activity, mileage_per_month)
